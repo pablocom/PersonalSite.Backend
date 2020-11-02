03bfef1d2b5de224503a6d1e0d5c13a3c14f4c44
pablocom <p.c.ramirez5150@gmail.com>
2020-11-02 22:27:21 +0100

    fix(IntegrationTests): corrects repository integration test to dont call service

--- src/PersonalSite.Domain.IntegrationTests/WhenSavingJobExperience.cs
+++ src/PersonalSite.Domain.IntegrationTests/WhenSavingJobExperience.cs
@@ -6,18 +6,11 @@ using PersonalSite.Domain.Model.JobExperienceAggregate;
 namespace PersonalSite.Domain.IntegrationTests
 {
     [TestFixture]
-    public class WhenCreatingJobExperience : PersonalSiteIntegrationTestBase
+    public class WhenSavingJobExperience : PersonalSiteIntegrationTestBase
     {
-        private IJobExperienceService _service;
-
-        protected override void AdditionalSetup()
-        {
-            _service = new JobExperienceService(Repository);
-        }
-
         [Test]
         [Ignore("WIP: creating Integration Test project.")]
-        public void JobExperienceIsPersisted()
+        public void IsPersisted()
         {
             var company = "Ryanair";
             var description = "Software Engineer";
@@ -25,7 +18,7 @@ namespace PersonalSite.Domain.IntegrationTests
             var endDate = new DateTime(2021, 07, 01);
             var techStack = new[] { ".Net", "MySQL" };
 
-            _service.CreateJobExperience(company, description, startDate, endDate, techStack);
+            Repository.Add(new JobExperience(company, description, new JobPeriod(startDate, endDate), techStack));
             SaveChanges();
 
             var jobExperience = Repository.GetAllJobExperiences().Single();
