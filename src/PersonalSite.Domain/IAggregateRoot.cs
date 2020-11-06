8be2094ccc10ec1903480f423b9875c07d5b40f2
Pablo <p.c.ramirez5150@gmail.com>
2020-11-06 10:21:12 +0100

    refactor(Domain): Renames domain repository and IAggregate root summaries

--- src/PersonalSite.Domain/IAggregateRoot.cs
+++ src/PersonalSite.Domain/IAggregateRoot.cs
@@ -1,7 +1,7 @@
 ﻿namespace PersonalSite.Domain
 {
     /// <summary>
-    /// Interface <c>AggregateRoot</c> is a marker for AggregateRoot entities of the bounded context
+    /// Marker for AggregateRoot entities of the bounded context
     /// </summary>
     public interface IAggregateRoot
     { }
--- src/PersonalSite.Domain/IDomainRepository.cs
+++ src/PersonalSite.Domain/IDomainRepository.cs
@@ -1,7 +1,7 @@
 ﻿namespace PersonalSite.Domain
 {
     /// <summary>
-    /// Interface <c>IDomainRepository</c> to mark domain repository of an aggregate root
+    /// Marks domain repository of an aggregate root
     /// </summary>
     /// <typeparam name="T">Type of aggregate root of repository</typeparam>
     public interface IDomainRepository<T> where T : IAggregateRoot
