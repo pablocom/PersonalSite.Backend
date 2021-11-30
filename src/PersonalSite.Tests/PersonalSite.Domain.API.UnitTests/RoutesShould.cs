2910315ff54d0a77d010d758cc8bd4478f4dff5d
rodriguezl <rodriguezl@ryanair.com>
2021-11-30 16:43:29 +0100

    unit tests over web api routes

---
+++ src/PersonalSite.Tests/PersonalSite.Domain.API.UnitTests/RoutesShould.cs
@@ -0,0 +1,45 @@
+﻿using Moq;
+using MyTested.AspNetCore.Mvc;
+using NUnit.Framework;
+using PersonalSite.WebApi.Controllers;
+using PersonalSite.WebApi.Dtos;
+
+namespace PersonalSite.API.UnitTests
+{
+    [TestFixture]
+    public class RoutesShould
+    {
+        [Test]
+        public void MapToHealthCheck()
+        {
+            MyMvc
+                .Routing()
+                .ShouldMap(request => request
+                    .WithMethod(HttpMethod.Get)
+                    .WithLocation("/HealthCheck"))
+                .To<HealthCheckController>();
+        }
+
+        [Test]
+        public void MapToGetAllJobExperience()
+        {
+            MyMvc
+                .Routing()
+                .ShouldMap(request => request
+                    .WithMethod(HttpMethod.Get)
+                    .WithLocation("/JobExperience"))
+                .To<JobExperienceController>();
+        }
+
+        [Test]
+        public void MapToCreateJobExperience()
+        {
+            MyMvc
+                .Routing()
+                .ShouldMap(request => request
+                    .WithMethod(HttpMethod.Post)
+                    .WithLocation("/JobExperience"))
+                .To<JobExperienceController>(x => x.Create(It.IsAny<CreateJobExperienceDto>()));
+        }
+    }
+}
