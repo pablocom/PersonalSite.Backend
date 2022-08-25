a921de07c15edd7a73a7c67c18cff8826372529c
Pablo <p.c.ramirez5150@gmail.com>
2022-08-25 18:57:40 +0200

    Add ErrorHandlingEndpoints

---
+++ src/PersonalSite.WebApi/Endpoints/ErrorHandlingEndpoints.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.Extensions.Logging;
+using PersonalSite.WebApi.Endpoints.Internal;
+using System.Net;
+
+namespace PersonalSite.WebApi.Endpoints;
+
+public class ErrorHandlingEndpoints : IEndpoints
+{
+    public const string BasePath = "/error";
+
+    public static void DefineEndpoints(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/error", HandleError)
+            .ExcludeFromDescription();
+    }
+
+    private static IResult HandleError(HttpContext httpContext, ILogger<ErrorHandlingEndpoints> logger)
+    {
+        var exceptionContext = httpContext.Features.Get<IExceptionHandlerFeature>();
+        logger.LogError(exceptionContext.Error.Message);
+
+        return Results.Problem(
+            statusCode: (int)HttpStatusCode.InternalServerError);        
+    }
+}
