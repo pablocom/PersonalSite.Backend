076735879930c4b5463d5f9e88fbc657385c5518
Seymour Poler <seymourpoler@gmail.com>
2023-01-15 01:03:43 +0100

    Setup xUnit #22

--- src/PersonalSite.Tests/PersonalSite.FunctionalTests/WhenCreatingAndRetrievingJobExperiences.cs
+++ src/PersonalSite.Tests/PersonalSite.FunctionalTests/WhenCreatingAndRetrievingJobExperiences.cs
@@ -6,16 +6,15 @@ using System.Text.Json;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.Extensions.DependencyInjection;
-using NUnit.Framework;
-using NUnit.Framework.Interfaces;
 using PersonalSite.Domain.Dtos;
 using PersonalSite.Persistence;
 using PersonalSite.WebApi.Dtos;
 using PersonalSite.WebApi.Converters;
+using Xunit;
 
 namespace PersonalSite.FunctionalTests;
 
-public class WhenCreatingAndRetrievingJobExperiences
+public class WhenCreatingAndRetrievingJobExperiences : IDisposable
 {
     private static readonly JsonSerializerOptions JsonSerializerOptions = new()
     {
@@ -26,21 +25,20 @@ public class WhenCreatingAndRetrievingJobExperiences
     private HttpClient _client = default!;
     private readonly WebApplicationFactory<PersonalSite.WebApi.IAssemblyMarker> _applicationFactory = new();
 
-    [SetUp]
-    public void Setup()
+    public WhenCreatingAndRetrievingJobExperiences()
     {
         _client = _applicationFactory.CreateClient();
     }
 
-    [Test]
+    [Fact]
     public async Task SingleJobExperienceIsCreatedAndRetrieved()
     {
         var response = await _client.GetAsync("/jobExperiences");
 
-        Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
         var jobExperiences = await response.Content.ReadFromJsonAsync<JobExperienceDto[]>(JsonSerializerOptions);
-        Assert.IsNotNull(jobExperiences);
-        Assert.IsEmpty(jobExperiences!);
+        Assert.NotNull(jobExperiences);
+        Assert.Empty(jobExperiences!);
 
         var createJobExperienceDto = new CreateJobExperienceDto(
             "Ryanair",
@@ -49,31 +47,28 @@ public class WhenCreatingAndRetrievingJobExperiences
             new DateOnly(2020, 5, 1),
             new[] { ".Net Core", "NSubstitute" });
         var createResponse = await _client.PostAsJsonAsync("/jobExperiences", createJobExperienceDto, JsonSerializerOptions);
-        Assert.That(createResponse.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+        Assert.Equal(HttpStatusCode.OK, createResponse.StatusCode);
 
         var secondResponse = await _client.GetAsync("jobExperiences");
-        Assert.That(secondResponse.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+        Assert.Equal(HttpStatusCode.OK, secondResponse.StatusCode);
         var json = await secondResponse.Content.ReadAsStringAsync();
         var secondJobExperiences = JsonSerializer.Deserialize<JobExperienceDto[]>(json, JsonSerializerOptions);
-        Assert.That(secondJobExperiences, Has.Length.EqualTo(1));
-        Assert.That(secondJobExperiences![0].Company, Is.EqualTo(createJobExperienceDto.Company));
-        Assert.That(secondJobExperiences[0].Description, Is.EqualTo(createJobExperienceDto.Description));
-        Assert.That(secondJobExperiences[0].JobPeriodEnd, Is.EqualTo(createJobExperienceDto.JobPeriodEnd));
-        Assert.That(secondJobExperiences[0].JobPeriodStart, Is.EqualTo(createJobExperienceDto.JobPeriodStart));
-        Assert.That(secondJobExperiences[0].TechStack, Is.EqualTo(createJobExperienceDto.TechStack));
+        Assert.Single(secondJobExperiences);
+        Assert.Equal(secondJobExperiences![0].Company, createJobExperienceDto.Company);
+        Assert.Equal(secondJobExperiences[0].Description, createJobExperienceDto.Description);
+        Assert.Equal(secondJobExperiences[0].JobPeriodEnd, createJobExperienceDto.JobPeriodEnd);
+        Assert.Equal(secondJobExperiences[0].JobPeriodStart, createJobExperienceDto.JobPeriodStart);
+        Assert.Equal(secondJobExperiences[0].TechStack, createJobExperienceDto.TechStack);
     }
 
-    [TearDown]
-    public async Task Teardown()
+    public void Dispose()
     {
-        if (TestContext.CurrentContext.Result.Outcome.Status == TestStatus.Passed)
-        {
-            await using var scope = _applicationFactory.Services.CreateAsyncScope();
-            var dbContext = scope.ServiceProvider.GetRequiredService<PersonalSiteDbContext>();
-            dbContext.JobExperiences.RemoveRange(dbContext.JobExperiences);
-            dbContext.IntegrationEvents.RemoveRange(dbContext.IntegrationEvents);
-            await dbContext.SaveChangesAsync();
-        }
+        using var scope = _applicationFactory.Services.CreateScope();
+        var dbContext = scope.ServiceProvider.GetRequiredService<PersonalSiteDbContext>();
+        dbContext.JobExperiences.RemoveRange(dbContext.JobExperiences);
+        dbContext.IntegrationEvents.RemoveRange(dbContext.IntegrationEvents);
+        dbContext.SaveChanges();
+
         _applicationFactory.Dispose();
     }
 }
